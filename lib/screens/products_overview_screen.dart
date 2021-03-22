e2657a5bc6447b358be18978a02edec686f4fbbf
Aryan Agarwal <aryankush025@gmail.com>
2021-03-23 00:24:39 +0530

    Favourites filter added

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favourites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  bool _showFavouritesOnly = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +22,32 @@ class ProductOverviewScreen extends StatelessWidget {
         title: Text(
           'My shop',
         ),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favourites) {
+                  _showFavouritesOnly = true;
+                } else {
+                  _showFavouritesOnly = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.Favourites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showFavouritesOnly),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,8 +12,6 @@ class ProductItem extends StatelessWidget {
       listen: false,
     );
 
-    print('object');
-
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
