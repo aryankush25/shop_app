34811d010df251d82d3744fe7a51f298e464562c
Aryan Agarwal <aryankush025@gmail.com>
2021-03-27 17:55:39 +0530

    Form and validation added

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../providers/product.dart';
+
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
 
@@ -8,14 +10,176 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
+  String _title = '';
+  double _price = 0;
+  String _description = '';
+  String _imageUrl = '';
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _imageUrlController.dispose();
+    super.dispose();
+  }
+
+  void _saveForm() {
+    final isValid = _formkey.currentState.validate();
+
+    if (isValid) {
+      _formkey.currentState.save();
+
+      var _editedProduct = Product(
+        id: null,
+        title: _title,
+        description: _description,
+        imageUrl: _imageUrl,
+        price: _price,
+      );
+
+      print(_editedProduct);
+      print(_title);
+      print(_price);
+      print(_description);
+      print(_imageUrl);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Product'),
+        actions: [
+          IconButton(
+            onPressed: _saveForm,
+            icon: Icon(Icons.save),
+          ),
+        ],
       ),
-      body: Container(
-        child: Text('Edit'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formkey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    // _priceFocusNode.requestFocus();
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  onSaved: (value) {
+                    _title = value;
+                  },
+                  validator: (value) {
+                    if (value.length < 2) {
+                      return 'This is wrong!';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Price',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode,
+                  onSaved: (value) {
+                    _price = double.parse(value);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a value!';
+                    }
+
+                    if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  maxLines: 3,
+                  keyboardType: TextInputType.multiline,
+                  onSaved: (value) {
+                    _description = value;
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'This is wrong!';
+                    }
+
+                    return null;
+                  },
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(
+                        top: 8,
+                        right: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: _imageUrlController.text.isEmpty
+                          ? Text('Enter a URL!')
+                          : FittedBox(
+                              child: Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Image URL',
+                        ),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        onEditingComplete: () {
+                          setState(() {});
+                        },
+                        onSaved: (value) {
+                          _imageUrl = value;
+                        },
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'This is wrong!';
+                          }
+
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
