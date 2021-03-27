1306baeac33fa5d684319debefc4524c1b821817
Aryan Agarwal <aryankush025@gmail.com>
2021-03-27 16:25:40 +0530

    Edit product screen

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Container(
+        child: Text('Edit'),
+      ),
+    );
+  }
+}
